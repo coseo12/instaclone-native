e860d7c3c836edef43743a4630d2cda5a3c197d1
coseo12 <momo_12@naver.com>
2021-05-26 18:45:05 +0900

    init

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => setLoading(false);
+  const preload = () => {
+    const fontToLoad = [Ionicons.font];
+    const fontPromises = fontToLoad.map(font => Font.loadAsync(font));
+    const imagesToLoad = [
+      require('./assets/instagram-logo.png'),
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/2560px-Instagram_logo.svg.png',
+    ];
+    const imagePromises = imagesToLoad.map(image => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onFinish={onFinish}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,19 @@
+# Instaclone native app
+
+React Native
+
+## NPM Package
+
+- [x] @react-navigation/native
+
+## Expo Package
+
+- [x] expo-app-loading
+- [x] expo-font
+- [x] expo-asset
+- [x] @react-navigation/native
+- [x] react-native-gesture-handler
+- [x] react-native-reanimated
+- [x] react-native-screens
+- [x] react-native-safe-area-context
+- [x] @react-native-community/masked-view
